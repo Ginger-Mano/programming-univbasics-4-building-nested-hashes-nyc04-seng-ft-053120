17cd75e7789f47153cbe7fbb7aa40912efdb6b06
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-08 14:23:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,7 @@ end
 
 def monopoly_with_second_tier
   hash = {:railroads => {}}
-  if hash.has_key?(:pieces)
-    hash[:railroads][:pieces] += 1
-  else
-    hash[:railroads].merge!({:pieces => 4})
+  hash[:railroads][0].merge!({:pieces => 4})
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more

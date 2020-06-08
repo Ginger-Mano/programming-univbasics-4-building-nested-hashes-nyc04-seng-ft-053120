37bdc9db0dbfdbef298855abd723738f4631417e
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-08 15:14:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,8 +7,7 @@ end
 
 def monopoly_with_second_tier
   hash = {:railroads => {}}
-  hash[:pieces] = 4
-  hash[:pieces]
+  hash[:railroads].store(pieces, 4)
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more

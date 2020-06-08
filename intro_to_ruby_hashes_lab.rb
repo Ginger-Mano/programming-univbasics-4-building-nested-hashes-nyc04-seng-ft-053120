f0b885877013ad3b7d4fcd89ce05393c96705423
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-08 15:21:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,7 @@ end
 
 def monopoly_with_second_tier
   hash = {:railroads => {}}
-  hash[:pieces] = 4
-  hash[:rent_in_dollars] = {}
-  hash[:names] = {}
-  puts hash
+  puts hash.merge({pieces: 4, rent_in_dollars: {}, names: {}})
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more

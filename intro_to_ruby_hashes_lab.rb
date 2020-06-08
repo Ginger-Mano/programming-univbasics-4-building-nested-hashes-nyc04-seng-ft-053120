10bd2ce3157a16cc14596e9abec104e9889a0403
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-08 16:15:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  hash[:railroads] = {}
+  hash = {:railroads => {}}
   hash[:railroads][:pieces] = 4
   hash[:railroads][:rent_in_dollars] = {}
   hash[:railroads][:names] = {}
